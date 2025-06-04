f5805bd8acd814f4e646e29ec71f729b6c04edc1
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-06-04 06:55:34 +0200

    Updating project model

--- Models/Project.cs
+++ Models/Project.cs
@@ -7,7 +7,7 @@
         public string Description { get; set; }
         public string TechStack { get; set; }
         public string GithubUrl { get; set; }
-        public string DemoUrl { get; set; }
-        public string ImageUrl { get; set; }
+        public string? DemoUrl { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
