7440f14f0f5c5a166b07fde94ac410c7b934a630
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-06-10 22:05:47 +0200

    Career and Education model to display cv

---
+++ Models/Education.cs
@@ -0,0 +1,9 @@
+﻿namespace PortfolioApp.Models
+{
+    public class Education
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Period { get; set; }
+    }
+}
