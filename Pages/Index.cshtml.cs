7ad70e7613db8116840d9cca1f94ca00354e1745
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-06-11 23:20:56 +0200

    Displaying Careers and Educations on homepage

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,21 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PortfolioApp.Data;
+using PortfolioApp.Models;
 
 namespace PortfolioApp.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    private readonly PortfolioContext _context;
+    public List<Career> careers { get; set; }
+    public List<Education> educations { get; set; }
+    public IndexModel(ILogger<IndexModel> logger, PortfolioContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public void OnGet()
+    public async Task OnGetAsync()
     {
         var datetime = DateTime.Now;
         string greeting;
+
+
         if (datetime.Hour > 2 && datetime.Hour < 11)
         {
             greeting = "Guten Morgen! ";
@@ -35,5 +43,8 @@ public class IndexModel : PageModel
         greeting += "Willkommen zu meiner Portfolio Seite!";
 
         ViewData["Greeting"] = greeting;
+        this.careers = await _context.Careers.OrderByDescending(i => i.Period).ToListAsync();
+        this.educations = await _context.Educations.OrderByDescending(i => i.Period).ToListAsync();
+        
     }
 }
